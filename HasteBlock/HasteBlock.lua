32e00021a990d7589c954912133371b5e2da0106
shadowed.wow <shadowed.wow@7083c368-1f31-0410-96f0-4f2793cbc738>
2007-08-28 06:05:43 +0000

    * Updated HA * Added Revision keywords * Changed configuration to not show ranged/offhand attack speed for casters and not show spell haste for non-mana users * Removed haste/speed wording for the LB, wasn't really needed just wasted space * Added in show LB, background opacity, border opacity options * Fixed the LB having transparent background * Cleaned up code * Added profiles so it's per character settings * Fixed LB position not saving

--- HasteBlock/HasteBlock.lua
+++ HasteBlock/HasteBlock.lua
@@ -10,12 +10,20 @@ local mainHand
 local offHand
 local ranged
 
+local mhLink
+local ohLink
+local rangedLink
+
+local baseSpell = 0
+local currentSpell = 0
+local spellID
+
 function HasteBlock:OnLoad()
 	self.tooltip = CreateFrame("GameTooltip", "HBTooltip", UIParent, "GameTooltipTemplate")
 	self.tooltip:SetOwner(this, "ANCHOR_NONE")
-
-	if( not HasteBlockDB ) then HasteBlockDB = {} end
-
+	
+	if( not HasteBlockDB ) then HasteBlockDB = { lego = {} } end
+	
 	self.defaults = {
 		showRanged = true,
 		showMain = true,
@@ -23,15 +31,26 @@ function HasteBlock:OnLoad()
 		showSpell = true,
 		showOriginal = true,
 		showHaste = true,
+		lego = {
+			bgAlpha = 1.0,
+			borderAlpha = 1.0,
+			showText = true,
+			hidden = false,
+		}
 	}
-	self.db = setmetatable(HasteBlockDB, {__index=function(t,k) return self.defaults[k] end})
+	
+	local meta = {__index=function(t, k) return self.defaults[k] end}
 
-	if not self.db.lego then
-		self.db.lego = {}
-	end
+	self.db = setmetatable(HasteBlockDB, meta)
+	self.db.lego = setmetatable(HasteBlockDB.lego, meta)
 
-	LB = DongleStub("LegoBlock-Beta0"):New("HasteBlock", "----" )
-	LB:SetDB(self.db.lego)
+	LB = DongleStub("LegoBlock-Beta0"):New("HasteBlock", "----", nil, HasteBlockDB.lego )
+	
+	local r, g, b = LB:GetBackdropColor()
+	LB:SetBackdropColor(r, g, b, self.db.lego.bgAlpha)
+	
+	r, g, b = LB:GetBackdropBorderColor()
+	LB:SetBackdropBorderColor(r, g, b, self.db.lego.borderAlpha)
 
 	OH = DongleStub("OptionHouse-1.0")
 
@@ -39,50 +58,84 @@ function HasteBlock:OnLoad()
 	OHObj:RegisterCategory(L["General"], self, "CreateUI")	
 end
 
-function HasteBlock:Set(var, val)
-	self.db[var] = val
+function HasteBlock:ToggleBlock(val)
+	if( val ) then
+		LB:Show()
+	else
+		LB:Hide()
+	end
+end
+
+function HasteBlock:OpacityChanged(val, lego, key)
+	if( key == "bgAlpha" ) then
+		local r, g, b = LB:GetBackdropColor()
+		LB:SetBackdropColor(r, g, b, self.db.lego.bgAlpha)
+
+	elseif( key == "borderAlpha" ) then
+		local r, g, b = LB:GetBackdropBorderColor()
+		LB:SetBackdropBorderColor(r, g, b, self.db.lego.borderAlpha)
+	end
+end
+
+function HasteBlock:Set(val, ...)
+	if( select("#", ...) == 1 ) then
+		self.db[select(1, ...)] = val
+	else
+		self.db[select(1, ...)][select(2, ...)] = val
+	end
+
 	self:SpeedChanged()
 end
 
-function HasteBlock:Get(var)
-	return self.db[var]
+function HasteBlock:Get(...)
+	if( select("#", ...) == 1 ) then
+		return self.db[select(1, ...)]
+	end
+	
+	return self.db[select(1, ...)][select(2, ...)]
 end
 
 function HasteBlock:CreateUI()
-	local config = {
-		{	type = "check",
-		text = L["Show original attack speed"],
-		default = true,
-		var = "showOriginal",
-	},
-	{	type = "check",
-	text = L["Show haste percentage"],
-	default = true,
-	var = "showHaste",
-},
-{	type = "check",
-text = L["Show mainhand attack speed"],
-default = true,
-var = "showMain",
-		},
-		{	type = "check",
-		text = L["Show offhand attack speed"],
-		default = true,
-		var = "showOff",
-	},
-	{	type = "check",
-	text = L["Show ranged attack speed"],
-	default = true,
-	var = "showRanged",
-},
-{	type = "check",
-text = L["Show spell haste percentage"],
-default = true,
-var = "showSpell",
-		},
-	}
+	local config = {}
+	-- Mana class (Spell Haste)
+	if( UnitPowerType("player") == 0 ) then
+		table.insert(config, {type = "check", text = L["Show spell haste percentage"], default = true, var = "showSpell"})
+		table.insert(config, {type = "check", text = L["Show original attack speed"], default = true, var = "showOriginal"})
+		table.insert(config, {type = "check", text = L["Show haste percentage"], default = true, var = "showHaste"})
+		table.insert(config, {type = "check", text = L["Show mainhand attack speed"], default = true, var = "showMain"})
+		
+	-- Melee/Ranged (Other haste)
+	else
+		table.insert(config, {type = "check", text = L["Show original attack speed"], default = true, var = "showOriginal"})
+		table.insert(config, {type = "check", text = L["Show haste percentage"], default = true, var = "showHaste"})
+		table.insert(config, {type = "check", text = L["Show mainhand attack speed"], default = true, var = "showMain"})
+		table.insert(config, {type = "check", text = L["Show offhand attack speed"], default = true, var = "showOff"})
+		table.insert(config, {type = "check", text = L["Show ranged attack speed"], default = true, var = "showRanged"})
+	end
+	
+	table.insert(config, {type = "label", color = { r = 1, g = 1, b = 1 }, text = L["LegoBlock Settings"]})
+	table.insert(config, {type = "check", text = L["Show Haste LegoBlock"], default = true, onSet = "ToggleBlock", var = { "lego", "hidden" }})
+	--table.insert(config, {type = "check", text = L["Show header text"], default = true, var = { "lego", "showText" }})
+	--table.insert(config, {type = "slider", format = L["Scale: %d%%"], var = { "lego", "scale" }, default = 1.0, min = 0.0, max = 2.0})
+	table.insert(config, {type = "slider", format = L["Background Opacity: %d%%"], var = { "lego", "bgAlpha" }, onSet = "OpacityChanged", default = 1.0})
+	table.insert(config, {type = "slider", format = L["Border Opacity: %d%%"], var = { "lego", "borderAlpha" }, onSet = "OpacityChanged", default = 1.0})
 
 	return DongleStub("HousingAuthority-1.0"):CreateConfiguration(config, {handler = self, set = "Set", get = "Get"})
+end
+
+function HasteBlock:ScanSpell(id)
+	HasteBlock.tooltip:ClearLines()
+	HasteBlock.tooltip:SetSpell(id, BOOKTYPE_SPELL)
+
+	for i=1, HasteBlock.tooltip:NumLines() do
+		local line = getglobal("HBTooltipTextLeft" ..i):GetText();
+		if( line ) then
+			local speed = string.match(line, "(.+)" .. L["sec cast"])
+			if( speed ) then
+				return tonumber(string.trim(speed))
+			end
+		end
+	end
 end
 
 function HasteBlock:ScanItem(slot)
@@ -108,57 +161,60 @@ end
 
 function HasteBlock:CalculateHaste(id, text, speed, origSpeed)
 	-- Spell Haste: 20.5%
-	if( not speed ) then
-		local bonus = GetCombatRatingBonus(id)
-		if( bonus ~= 0 ) then
-			display = display .. text .. string.format("%.2f", bonus) .. "\n"
+	if( id == CR_HASTE_SPELL ) then
+		local speed = (1 - speed / origSpeed) * 100)
+		if( speed > 0.0 ) then
+			display = display .. text .. string.format(" %.2f%%", speed) .. "\n"
 		end
-		-- Main Hand: 1.82 (2.6 -30%)
+		
+	-- Main Hand: 1.82 (2.6 -30%)
 	elseif( self.db.showOriginal and self.db.showHaste and origSpeed ~= speed ) then
-		display = display .. text .. string.format("%.2f (%.2f -%.0f%%)", speed, origSpeed, (1-speed/origSpeed)*100) .. "\n"
-		-- Main Hand: 1.82 (-30%)
+		display = display .. text .. string.format(" %.2f (%.2f -%.0f%%)", speed, origSpeed, (1 - speed / origSpeed) * 100) .. "\n"
+	-- Main Hand: 1.82 (-30%)
 	elseif( self.db.showHaste and origSpeed ~= speed ) then
-		display = display .. text .. string.format("%.2f (-%.0f%%)", speed, (1-speed/origSpeed)*100) .. "\n"
-		-- Main Hand: 1.82 (2.6)
+		display = display .. text .. string.format(" %.2f (-%.0f%%)", speed, (1 - speed / origSpeed) * 100) .. "\n"
+	
+	-- Main Hand: 1.82 (2.6)
 	elseif( self.db.showOriginal and origSpeed ~= speed ) then
-		display = display .. text .. string.format("%.2f (%.2f)", speed, origSpeed) .. "\n"
+		display = display .. text .. string.format(" %.2f (%.2f)", speed, origSpeed) .. "\n"
 	else
-		display = display .. text .. string.format("%.2f", speed) .. "\n"
+		display = display .. text .. string.format(" %.2f", speed) .. "\n"
 	end
 end
 
 function HasteBlock:SpeedChanged()
 	display = ""
-	if( self.db.showRanged ) then
-		self:CalculateHaste(CR_HASTE_RANGED, L["Ranged Speed:"], (UnitRangedDamage("player")), ranged)
+	
+	-- Ranged weapon
+	if( self.db.showRanged and ranged  ) then
+		self:CalculateHaste(CR_HASTE_RANGED, L["Ranged:"], (UnitRangedDamage("player")), ranged)
 	end
-
+	
+	-- Mainhand
 	local main, off = UnitAttackSpeed("player")
 	if( self.db.showMain and mainHand ) then
-		self:CalculateHaste(CR_HASTE_MELEE, L["Mainhand Speed:"], main, mainHand)
+		self:CalculateHaste(CR_HASTE_MELEE, L["Mainhand:"], main, mainHand)
 	end
-
+	
+	-- Offhand
 	if( self.db.showOff and offHand ) then
-		self:CalculateHaste(CR_HASTE_MELEE, L["Offhand Speed:"], off, offHand)
+		self:CalculateHaste(CR_HASTE_MELEE, L["Offhand:"], off, offHand)
 	end
 
-	if( self.db.showSpell ) then
-		self:CalculateHaste(CR_HASTE_SPELL, L["Spell Haste:"])
+	-- Spells
+	if( self.db.showSpell and baseSpell ) then
+		self:CalculateHaste(CR_HASTE_SPELL, L["Spell:"], baseSpell, currentSpell)
 	end
 
 	if( display ~= "" ) then
 		LB:SetText(string.trim(display))
 		LB.Text:SetWidth(LB:GetWidth())
+		LB:SetHeight(LB.Text:GetHeight() + 15)
 		LB:Show()
 	else
 		LB:Hide()
 	end
 end
-
-local mhID = CharacterMainHandSlot:GetID()
-local ohID = CharacterSecondaryHandSlot:GetID()
-local rangedID = CharacterRangedSlot:GetID()
-local mhlink, ohlink, rangedlink
 
 local frame = CreateFrame("Frame")
 frame:SetScript("OnEvent", function(self, event, arg1)
@@ -167,33 +223,74 @@ frame:SetScript("OnEvent", function(self, event, arg1)
 		self:UnregisterEvent("ADDON_LOADED")
 
 	elseif( event == "UNIT_INVENTORY_CHANGED" and arg1 == "player" ) then
-		local newmh, newoh, newranged = GetInventoryItemLink("player", mhID), GetInventoryItemLink("player", ohID), GetInventoryItemLink("player", rangedID)
-		if newmh ~= mhlink then
-			mainHand = HasteBlock:ScanItem(mhID)
-			mhlink = newmh
+		local newMh = GetInventoryItemLink("player", CharacterMainHandSlot:GetID())
+		local newOh = GetInventoryItemLink("player", CharacterSecondaryHandSlot:GetID())
+		local newRanged = GetInventoryItemLink("player", CharacterRangedSlot:GetID())
+		
+		if( newMh ~= mhLink ) then
+			mainHand = HasteBlock:ScanItem(CharacterMainHandSlot:GetID())
+			mhLink = newMh
 		end
-		if newoh ~= ohlink then
-			offHand = HasteBlock:ScanItem(ohID)
-			ohlink = newoh
+		
+		if( newOh ~= ohLink ) then
+			offHand = HasteBlock:ScanItem(CharacterSecondaryHandSlot:GetID())
+			ohLink = newOh
 		end
-		if newranged ~= rangedlink then
-			ranged = HasteBlock:ScanItem(rangedID)
-			rangedlink = newranged
+		
+		if( newRanged ~= rangedLink ) then
+			ranged = HasteBlock:ScanItem(CharacterRangedSlot:GetID())
+			rangedLink = newRanged
 		end
+				
 		HasteBlock:SpeedChanged()
 
 	elseif( event == "PLAYER_LOGIN" ) then
-		mhlink, ohlink, rangedlink = GetInventoryItemLink("player", mhID), GetInventoryItemLink("player", ohID), GetInventoryItemLink("player", rangedID)
-		mainHand = HasteBlock:ScanItem(mhID)
-		offHand = HasteBlock:ScanItem(ohID)
-		ranged = HasteBlock:ScanItem(rangedID)
+		mhLink = GetInventoryItemLink("player", CharacterMainHandSlot:GetID())
+		ohLink = GetInventoryItemLink("player", CharacterSecondaryHandSlot:GetID())
+		rangedLink = GetInventoryItemLink("player", CharacterRangedSlot:GetID())
+		
+		mainHand = HasteBlock:ScanItem(CharacterMainHandSlot:GetID())
+		offHand = HasteBlock:ScanItem(CharacterSecondaryHandSlot:GetID())
+		ranged = HasteBlock:ScanItem(CharacterRangedSlot:GetID())
+		
+		-- Scan a spell with a cast time if we're a mana user
+		-- so we can figure out how much haste we gained for spells
+		if( UnitPowerType("player") == 0 ) then
+			local total = 0
+			for i=1, GetNumSpellTabs() do
+				total = total + select(4, GetSpellTabInfo(i))
+			end
+			
+			-- Find the longest casting spell to make sure we
+			-- can actually get the correct haste info
+			for i=1, total do
+				local speed = HasteBlock:ScanSpell(i)
+				if( speed and speed > baseSpell ) then
+					baseSpell = speed
+					currentSpell = speed
+					spellID = i
+				end
+			end
+		end
+		
 		HasteBlock:SpeedChanged()
 
+	elseif( event == "PLAYER_LOGOUT" ) then
+		HasteBlockDB = HasteBlock.db
+
 	elseif( event == "UNIT_ATTACK_SPEED" ) then
+		if( spellID ) then
+			local speed = HasteBlock:ScanSpell(spellID)
+			if( speed ) then
+				currentSpell = speed
+			end
+		end
+
 		HasteBlock:SpeedChanged()
 	end
 end)
 frame:RegisterEvent("ADDON_LOADED")
 frame:RegisterEvent("PLAYER_LOGIN")
+frame:RegisterEvent("PLAYER_LOGOUT")
 frame:RegisterEvent("UNIT_ATTACK_SPEED")
 frame:RegisterEvent("UNIT_INVENTORY_CHANGED")
