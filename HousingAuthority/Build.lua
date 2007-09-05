2a424de81e1d50c827d0baa604e476851535d2ff
shadowed.wow <shadowed.wow@7083c368-1f31-0410-96f0-4f2793cbc738>
2007-09-05 07:53:46 +0000

    * Updated build script to actually work

--- HousingAuthority/Build.lua
+++ HousingAuthority/Build.lua
@@ -1,34 +1,3 @@
-local license = [=[--[[-------------------------------------------------------------------------
-  Copyright (c) 2006-2007, Dongle Development Team
-  All rights reserved.
-
-  Redistribution and use in source and binary forms, with or without
-  modification, are permitted provided that the following conditions are
-  met:
-
-      * Redistributions of source code must retain the above copyright
-        notice, this list of conditions and the following disclaimer.
-      * Redistributions in binary form must reproduce the above
-        copyright notice, this list of conditions and the following
-        disclaimer in the documentation and/or other materials provided
-        with the distribution.
-      * Neither the name of the Dongle Development Team nor the names of
-        its contributors may be used to endorse or promote products derived
-        from this software without specific prior written permission.
-
-  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
-  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
-  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
-  A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
-  OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
-  SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
-  LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
-  DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
-  THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
-  (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
-  OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
----------------------------------------------------------------------------]]]=]
-
 -- Determine the platform
 local platform
 local copy = "cp"
@@ -66,14 +35,8 @@ function buildFile(dirname, filename)
   %s
 ---------------------------------------------------------------------------]]]=]
 
-	ofile:write(license .. "\n")
-
-	local count = 0
-	for line in io.lines("DongleStub.lua") do
-		count = count + 1
-		if count > 5 then
-			ofile:write(line .. "\n")
-		end
+	for line in io.lines("LibStub.lua") do
+		ofile:write(line .. "\n")
 	end
 
    	ofile:write("\n"..string.format(section, "Begin Library Implementation").."\n")
@@ -101,5 +64,5 @@ buildFile("HousingAuthority", "HousingAuthority.lua")
 
 print("* Dumping a changelog")
 os.execute("svn log > HousingAuthority"..sep.."changelog.txt")
-os.execute("svn -m \"*Creating directory for HousingAutority-r" .. rev .. " mkdir https://shadowed-wow.googlecode.com/svn/tags/HousingAuthority-r" .. rev)
+os.execute("svn -m \"*Creating directory for HousingAutority-r" .. rev .. "\ mkdir https://shadowed-wow.googlecode.com/svn/tags/HousingAuthority-r" .. rev)
 os.execute("svn -m \"* Importing HousingAuthority-r"..rev.." addon\" import HousingAuthority https://shadowed-wow.googlecode.com/svn/tags/HousingAuthority-r" .. rev)
