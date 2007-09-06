e3a722f6f50076fda73ab23eb8618166b2ed9b72
shadowed.wow <shadowed.wow@7083c368-1f31-0410-96f0-4f2793cbc738>
2007-09-06 22:34:28 +0000

    * Changed HouseBuilder example to account for sliders messing up positioning, also added 4 total examples for testing instead of just one * Fixed positioning issues with sliders * Reduced height/width of buttons * Fixed entries in the dropdown all appearing as checked * Fixed color picker not showing up in front of the OptionHouse frame * Fixed column positioning code will work a lot better now * API HAS BEEN CHANGED. set, onSet, validate are now called like func(var, value) EVEN if var is a table. get has been changed to be called like func(var)

--- trunk/HousingAuthority/HouseBuilder.lua
+++ trunk/HousingAuthority/HouseBuilder.lua
@@ -4,27 +4,97 @@ local HouseBuild = {}
 local tables = {}
 
 function HouseBuild:Set(key, val)
+	--Debug( "Set [" .. tostring(key) .. "] [" .. tostring(val) .. "]" )
 	tables[key] = val
 end
 
 function HouseBuild:Get(key)
+	--Debug( "Get [" .. tostring(key) .. "]" )
 	return tables[key]
 end
 
 function HouseBuild:Check(var, val)
+	--Debug( "Check [" .. tostring(var) .. "] [" .. tostring(val) .. "]" )
 	return tonumber(val)
 end
 
 function HouseBuild:ButtonClicked(key)
-	DEFAULT_CHAT_FRAME:AddMessage( "Button with a key of " .. key .. " was pressed" )
+	--DEFAULT_CHAT_FRAME:AddMessage( "Button with a key of " .. tostring(key) .. " was pressed" )
 end
 
-function HouseBuild:CreateUI()
+-- NO GROUPS SINGLE COLUMN
+function HouseBuild:NoGroupSingleCol()
+	local config = {
+		{	type = "dropdown",
+			text = "Testing List",
+			default = "cat",
+			var = "selection",
+			help = "This is some help information regarding how to use this feature in.",
+			list = {{ "foo", "Foo" },
+				{ "apple", "Apple" },
+				{ "cat", "Cat" }}
+		},
+		{	type = "dropdown",
+			text = "Testing List 2",
+			var = "foobared",
+			default = "apple",
+			help = "This is some help information regarding how to use this feature in.",
+			list = {{ "Foo", "Foo" },
+				{ "Apple", "Apple" },
+				{ "Cat", "Cat" }}
+		},
+		{	type = "color",
+			text = "Colorize your stuff",
+			default = { r = 1, g = 1, b = 1 },
+			var = "background",
+			help = "Background color",
+		},
+		{	type = "input",
+			text = "Input Thing",
+			default = 50,
+			var = "val",
+			realTime = true,
+			numeric = false,
+			help = "Super special input, OMGHAIMIKMA",
+			validate = "Check",
+			error = "Error, \"%s\" is not a valid input",
+		},
+		{	type = "slider",
+			format = "Background opacity: %.2f",
+			help = "K, Thks, Bai", 
+			var = "opacity",
+			default = 1.0,
+			step = 0.01,
+			minText = "0%",
+			maxText = "200%",
+			min = 0.0,
+			max = 2.0,
+		},
+		{	type = "check",
+			text = "Enable a mod",
+			help = "FOR PONY",
+			default = true,
+			var = "enable",
+		},
+		{	type = "button",
+			text = "Enable something",
+			var = "foo",
+			set = "ButtonClicked",
+		},
+	};
+	
+	local HouseAuthority = LibStub:GetLibrary("HousingAuthority-1.1")
+	
+	return HouseAuthority:CreateConfiguration(config, { handler = self, set = "Set", get = "Get", columns = 1 } )
+end
+
+-- GROUPS USED SINGLE COLUMN
+function HouseBuild:GroupSingleCol()
 	local config = {
 		{	group = "Main Group",
 			type = "dropdown",
 			text = "Testing List",
-			default = "foo",
+			default = "cat",
 			var = "selection",
 			help = "This is some help information regarding how to use this feature in.",
 			list = {{ "foo", "Foo" },
@@ -32,11 +102,184 @@ function HouseBuild:CreateUI()
 				{ "cat", "Cat" }}
 		},
 		{	group = "Main Group",
+			type = "dropdown",
+			text = "Testing List 2",
+			var = "foobared",
+			default = "apple",
+			help = "This is some help information regarding how to use this feature in.",
+			list = {{ "Foo", "Foo" },
+				{ "Apple", "Apple" },
+				{ "Cat", "Cat" }}
+		},
+		{	group = "Main Group",
 			type = "color",
 			text = "Colorize your stuff",
 			default = { r = 1, g = 1, b = 1 },
 			var = "background",
 			help = "Background color",
+		},
+		{	group = "Main Group",
+			type = "input",
+			text = "Input Thing",
+			default = 50,
+			var = "val",
+			realTime = true,
+			numeric = false,
+			help = "Super special input, OMGHAIMIKMA",
+			validate = "Check",
+			error = "Error, \"%s\" is not a valid input",
+		},
+		{	group = "Secondary Group",
+			type = "slider",
+			format = "Background opacity: %.2f",
+			help = "K, Thks, Bai", 
+			var = "opacity",
+			default = 1.0,
+			step = 0.01,
+			minText = "0%",
+			maxText = "200%",
+			min = 0.0,
+			max = 2.0,
+		},
+		{	group = "Main Group",
+			type = "slider",
+			format = "Background opacity: %.2f",
+			help = "K, Thks, Bai", 
+			var = "opacity",
+			default = 1.0,
+			step = 0.01,
+			minText = "0%",
+			maxText = "200%",
+			min = 0.0,
+			max = 2.0,
+		},
+		{	group = "Secondary Group",
+			type = "check",
+			text = "Enable a mod",
+			help = "FOR PONY",
+			default = true,
+			var = "enable",
+		},
+		{	group = "Secondary Group",
+			type = "button",
+			text = "Enable something",
+			var = "foo",
+			set = "ButtonClicked",
+		},
+	};
+	
+	local HouseAuthority = LibStub:GetLibrary("HousingAuthority-1.1")
+	
+	return HouseAuthority:CreateConfiguration(config, { handler = self, set = "Set", get = "Get", columns = 1 } )
+end
+
+-- NO GROUPS 2 COLUMN VIEW USED
+function HouseBuild:NoGroupTwoCol()
+	local config = {
+		{	type = "dropdown",
+			text = "Testing List",
+			default = "cat",
+			var = "selection",
+			help = "This is some help information regarding how to use this feature in.",
+			list = {{ "foo", "Foo" },
+				{ "apple", "Apple" },
+				{ "cat", "Cat" }}
+		},
+		{	type = "dropdown",
+			text = "Testing List 2",
+			var = "foobared",
+			default = "apple",
+			help = "This is some help information regarding how to use this feature in.",
+			list = {{ "Foo", "Foo" },
+				{ "Apple", "Apple" },
+				{ "Cat", "Cat" }}
+		},
+		{	type = "color",
+			text = "Colorize your stuff",
+			default = { r = 1, g = 1, b = 1 },
+			var = "background",
+			help = "Background color",
+		},
+		{	type = "input",
+			text = "Input Thing",
+			default = 50,
+			var = "val",
+			realTime = true,
+			numeric = false,
+			help = "Super special input, OMGHAIMIKMA",
+			validate = "Check",
+			error = "Error, \"%s\" is not a valid input",
+		},
+		{	type = "slider",
+			format = "Background opacity: %.2f",
+			help = "K, Thks, Bai", 
+			var = "opacity",
+			default = 1.0,
+			step = 0.01,
+			minText = "0%",
+			maxText = "200%",
+			min = 0.0,
+			max = 2.0,
+		},
+		{	type = "check",
+			text = "Enable a mod",
+			help = "FOR PONY",
+			default = true,
+			var = "enable",
+		},
+		{	type = "button",
+			text = "Enable something",
+			var = "foo",
+			set = "ButtonClicked",
+		},
+	};
+	
+	local HouseAuthority = LibStub:GetLibrary("HousingAuthority-1.1")
+	
+	return HouseAuthority:CreateConfiguration(config, { handler = self, set = "Set", get = "Get", columns = 2 } )
+end
+
+-- GROUPS AND 2 COLUMN VIEW USED
+function HouseBuild:GroupTwoCol()
+	local config = {
+		{	group = "Main Group",
+			type = "dropdown",
+			text = "Testing List",
+			default = "cat",
+			var = "selection",
+			help = "This is some help information regarding how to use this feature in.",
+			list = {{ "foo", "Foo" },
+				{ "apple", "Apple" },
+				{ "cat", "Cat" }}
+		},
+		{	group = "Main Group",
+			type = "dropdown",
+			text = "Testing List 2",
+			var = "foobared",
+			default = "apple",
+			help = "This is some help information regarding how to use this feature in.",
+			list = {{ "Foo", "Foo" },
+				{ "Apple", "Apple" },
+				{ "Cat", "Cat" }}
+		},
+		{	group = "Main Group",
+			type = "color",
+			text = "Colorize your stuff",
+			default = { r = 1, g = 1, b = 1 },
+			var = "background",
+			help = "Background color",
+		},
+		{	group = "Main Group",
+			type = "slider",
+			format = "Background opacity: %.2f",
+			help = "K, Thks, Bai", 
+			var = "opacity",
+			default = 1.0,
+			step = 0.01,
+			minText = "0%",
+			maxText = "200%",
+			min = 0.0,
+			max = 2.0,
 		},
 		{	group = "Main Group",
 			type = "input",
@@ -85,7 +328,10 @@ function HouseBuild:Load()
 	local OptionHouse = LibStub:GetLibrary("OptionHouse-1.1")
 	local OHObj = OptionHouse:RegisterAddOn("HouseBuilder")
 	
-	OHObj:RegisterCategory("Test", self, "CreateUI")
+	OHObj:RegisterCategory("Groups 2 Col", self, "GroupTwoCol")
+	OHObj:RegisterCategory("Group Single Col", self, "GroupSingleCol")
+	OHObj:RegisterCategory("No Group 2 Col", self, "NoGroupTwoCol")
+	OHObj:RegisterCategory("No Group Single Col", self, "NoGroupSingleCol")
 end
 
 
