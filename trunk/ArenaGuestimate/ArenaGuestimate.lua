b5554f90999e9b711f5f8a42aa1f9229ce52955d
shadowed.wow <shadowed.wow@7083c368-1f31-0410-96f0-4f2793cbc738>
2007-10-12 00:04:30 +0000

    * Saved a list of aura/buffs that are castable on other players for detecting whos in the arena

--- trunk/ArenaGuestimate/ArenaGuestimate.lua
+++ trunk/ArenaGuestimate/ArenaGuestimate.lua
@@ -10,15 +10,15 @@ function ArenaGuess:Enable()
 	
 	self.db = self:InitializeDB("ArenaGuessDB", self.defaults)
 
-	self.cmd = self:InitializeSlashCommand(L["Arena Guestimate slash commands"], "ArenaGuess", "arenaguess", "aguess", "ag")
-	self.cmd:InjectDBCommands(self.db, "delete", "copy", "list", "set")
-	self.cmd:RegisterSlashHandler(L["ui - Opens the OptionHouse UI"], "ui", function() OH:Open("Arena Guestimate") end)
+	--self.cmd = self:InitializeSlashCommand(L["Arena Guestimate slash commands"], "ArenaGuess", "arenaguess", "aguess", "ag")
+	--self.cmd:InjectDBCommands(self.db, "delete", "copy", "list", "set")
+	--self.cmd:RegisterSlashHandler(L["ui - Opens the OptionHouse UI"], "ui", function() OH:Open("Arena Guestimate") end)
 	
 	self:RegisterEvent("ADDON_LOADED")
 
 	-- Register with OH
-	local ui = OH:RegisterAddOn("Arnea Guestimate", L["Arena Guess"], "Amarand", "r" .. tonumber( string.match( "$Revision: 231 $", "(%d+)" ) or 1 ) )
-	ui:RegisterCategory(L["General"], self, "CreateUI")
+	--local ui = OH:RegisterAddOn("Arnea Guestimate", L["Arena Guess"], "Amarand", "r" .. tonumber( string.match( "$Revision: 231 $", "(%d+)" ) or 1 ) )
+	--ui:RegisterCategory(L["General"], self, "CreateUI")
 end
 
 function ArenaGuess:CreateUI()
--- trunk/ArenaGuestimate/localization.enUS.lua
+++ trunk/ArenaGuestimate/localization.enUS.lua
@@ -1,5 +1,70 @@
+--[[
+-- WARRIOR
+Commanding Shout
+Battle Shout
+
+-- DRUID
+Gift of the Wild
+Mark of the Wild
+Thorns
+Lifebloom
+Regrowth
+Rejuvenation
+Leader of the Pack
+Moonkin Aura
+Tree of Life
+
+-- PRIEST
+Prayer of Fortitude
+Prayer of Shadow Protection
+Power Word: Fortitude
+Power Word: Shield
+Shadow Protection
+Renew
+
+-- PALADIN
+Sanctity Aura
+Blessing of Light
+Blessing of Might
+Blessing of Salvation
+Blessing of Wisdom
+Blessing of Sanctuary
+Blessing of KingsGreater Blessing of Sanctuary
+Greater Blessing of Light
+Greater Blessing of Might
+Greater Blessing of Salvation
+Greater Blessing of Wisdom
+Greater Blessing of Kings
+Concentration Aura
+Devotion Aura
+Fire Resistence Aura
+Frost Resistence Aura
+Retribution Aura
+Shadow Resistance Aura
+Crusader Aura
+
+-- MAGE
+Arcane Intellect
+Dampen Magic
+Amplify Magic
+Arcane Brilliance
+
+-- WARLOCK
+Unending Breath
+Detect Invisibility
+
+-- SHAMAN
+Grace of Air
+Strength of Earth
+Mana Spring
+Mana Tide
+
+-- HUNTER
+True Shot Aura
+]]
+
 ArenaGuessLocals = {
-	["Honest slash commands"] = "Arena Guestimate slash commands",
+	["Arena Guestimate slash commands"] = "Arena Guestimate slash commands",
 	["General"] = "General",
 	["ui - Opens the OptionHouse UI"] = "ui - Opens the OptionHouse UI",
 }
