a5c422118d489acab7ddf5e07457bbbad940fad5
shadowed.wow <shadowed.wow@7083c368-1f31-0410-96f0-4f2793cbc738>
2007-10-23 07:35:07 +0000

    * Hopefully added the framework required to expand this for Priest/Paladin/Shamans * Calculates Regrowth/Lifebloom/Rejuvenation HoT value, seems to be off by +/- 10-20 points, not exactly sure why yet.

--- Bishop/Bishop.lua
+++ Bishop/Bishop.lua
@@ -1,443 +1,188 @@
---[ Bishop
---This mod provides a method for healers to track statistics with regards to their healing
+Bishop = DongleStub("Dongle-1.1"):New("Bishop")
 
-local DEFAULT_OPTIONS = {}
-local RED = "|cffff0000"
-local GREEN = "|cff00ff00"
-local BLUE = "|cff0000ff"
-local YELLOW = "|cffffff00"
-local HEADER = YELLOW.."[Bishop] |cffffffff"
+local L = BishopLocals
 
---[[---------------------------------------------------------------------------------
-  Class Setup
-------------------------------------------------------------------------------------]]
+local healData = {}
+local spellData
+local talentData
+local rangedData
+local playerTalents = {}
+local equippedBonus = {}
 
-Bishop = AceAddon:new({
-	name		  = BISHOP.NAME,
-	description   = BISHOP.DESCRIPTION,
-	version		  = "1.0",
-	releaseDate   = "10-19-2005",
-	aceCompatible = "102",
-	author		  = "Cladhaire",
-	email		  = "cladhaire@gmail.com",
-	website		  = "http://watchdog.brokendreams.net/Bishop",
-	category	  = "other",
-	db			  = AceDatabase:new("BishopDB"),
-	defaults	  = DEFAULT_OPTIONS,
-	cmd			  = AceChatCmd:new(BISHOP.COMMANDS, BISHOP.CMD_OPTIONS),
-})
-
--- This method is for basic initialization stuff, like setting class variables,
--- performing data checks, and registering chat commands.
-function Bishop:Initialize()
-	
-	-- Save the tooltip as a shortcut
-	self.tooltip = BishopTooltip
-	
-	-- Create structure for saving spell information
-	self.spellInfo = {	name 		= nil,		-- The name of the spell
-						rank 		= nil,		-- The rank of the spell
-						target		= nil,		-- The target of the spell
-					}
-	
-	self.lastName = nil		-- The name of the last spell we scanned (tried to be cast)
-	self.lastRank = nil		-- The rank of the last spell we scanned (tried to be cast)
-	self.instant = nil		-- Only set to true if we've caught a spellcast
-	
-	-- Lets make some closures for our options
-	self.GetOpt = function(var) local v=self.db:get(self.profilePath, var) return v end
-	self.SetOpt = function(var, val) self.db:set(self.profilePath, var, val) end
-	self.TogOpt = function (var) return self.db:toggle(self.profilePath, var) end
-	self.TogMsg = function(text, val) self.cmd:status(text, val, ACEG_MAP_ONOFF) end
-	self.incVal = function(path, key, val) self.db:set(path, key, (self.db:get(path, key) or 0) + val) end
-	self.Get	= function(path, key) self.db:get(path, key) end
-end
-
--- The Enable() method checks to see if hello messages are turned on. If so, then
--- it registers the appropriate events that allow Bishop respond to zone changes.
 function Bishop:Enable()
-	
-	-- Register for events
-	self:RegisterEvent("SPELLCAST_START")
-	self:RegisterEvent("SPELLCAST_FAILED")
-	self:RegisterEvent("SPELLCAST_INTERRUPTED")
-	self:RegisterEvent("SPELLCAST_STOP")
-	self:RegisterEvent("SPELLCAST_CHANNEL_START")
-	
-	self:RegisterEvent("UNIT_INVENTORY_CHANGED")
-	self:RegisterEvent("LEARNED_SPELL_IN_TAB", "ParseSpellbook")
-
-	self:ParseSpellbook()
-	self:UNIT_INVENTORY_CHANGED(true)
-	
-	-- Healing spells on anyone in raid/party
+	self:RegisterEvent("CHAT_MSG_SPELL_PERIODIC_FRIENDLYPLAYER_BUFFS")
+	self:RegisterEvent("UNIT_SPELLCAST_SUCCEEDED")
+	self:RegisterEvent("UNIT_SPELLCAST_SENT")
+	--self:RegisterEvent("UNIT_SPELLCAST_STOP")
+	--self:RegisterEvent("UNIT_SPELLCAST_INTERRUPTED")
 	self:RegisterEvent("CHAT_MSG_SPELL_SELF_BUFF")
-	self:RegisterEvent("CHAT_MSG_SPELL_PERIODIC_SELF_BUFFS")
-	self:RegisterEvent("CHAT_MSG_SPELL_PERIODIC_PARTY_BUFFS", "HOT_FRIENDLY")
-	self:RegisterEvent("CHAT_MSG_SPELL_PERIODIC_FRIENDLYPLAYER_BUFFS", "HOT_FRIENDLY")
+	self:RegisterEvent("CONFIRM_TALENT_WIPE", "ScanPlayerTalents")
+	self:RegisterEvent("CHARACTER_POINTS_CHANGED", "ScanPlayerTalents")
+	self:RegisterEvent("UNIT_INVENTORY_CHANGED", "ScanPlayerRanged")
 	
-	self:Hook("CastSpell")
-	self:Hook("CastSpellByName")
-	self:Hook("UseAction")
-	self:Hook("SpellTargetUnit")
-	self:Hook("TargetUnit")
-	self:Hook("SpellStopTargeting")
-	self:Hook("CameraOrSelectOrMoveStart")
-end
-
---[[---------------------------------------------------------------------------------
-  Event Handling
-------------------------------------------------------------------------------------]]
-
-function Bishop:SPELLCAST_START()
-	-- When this fires we want to grab the last CastSpell/ByName/UseAction information
-	-- and save that information, so we can wait for the chat message (for processing)
-	-- arg1 == the spell name
+	spellData, talentData, rangedData = BishopData["Load" .. select(2, UnitClass("player"))]()
+	rangedData = rangedData or {}
 	
-	self:debug(GREEN.."SPELLCAST_START()")
-	
-	if not arg1 == self.lastName then
-		self:debug("Issue in SPELLCAST_START.  ("..tostring(arg1).." ~= "..tostring(self.lastName)..")")
+	-- Failed to load any data, exit quickly
+	if( not spellData ) then
+		self:Disable()
 		return
 	end
-
-	self:SetSpell(self.lastName, self.lastRank, self.lastTarget)
+	
+	self:ScanPlayerTalents()
+	self:ScanPlayerRanged()
 end
 
-function Bishop:SPELLCAST_FAILED()
-	-- This event will fire when we have a spell that can't be cast (invalid, more powerful)
-	-- or when we're moving when we attempt to cast.  This needs to clear all saved
-	-- variables whenever it fires.
-	
-	self:debug(RED.."SPELLCAST_FAILED()")
-	
-	self:ClearData()
+function Bishop:Disable()
+	self:UnregisterAllEvents()
 end
 
-function Bishop:SPELLCAST_INTERRUPTED()
-	-- This event will fire as an error message, but doesn't truly signify anything:
-	-- Fires when you move while casting (after SPELLCAST_STOP)  Shouldn't need to do anything
-	-- with this event, but its probably a good idea to clear the saved data when it drops.
+-- Check if we have any healing bonuses applied
+function Bishop:ScanPlayerRanged()
+	equippedBonus.spell = nil
+	equippedBonus.healing = nil
 	
-	self:debug(YELLOW.."SPELLCAST_INTERRUPTED()")
-	
-	self:ClearData()
-end
-
-function Bishop:SPELLCAST_STOP()
-	-- This is an important event, it fires on occasion when spells fail, but also when spells
-	-- finish casting.  It is the only event that fires for instant cast spells.
-	-- Fires when moving while casting a spell (obviously won't fire for instant casts)
-	-- Fires immediately after SPELLCAST_CHANNEL_START when casting a channeled spell
-	-- Fires when finished casting a spell
-	
-	-- At this point, we expect SetSpell to have been called.  If this is an instant
-	-- cast spell, self.instant will be true, otherwise it won't.  This event is only
-	-- truly used for instant cast spells. 
-	
-	-- At this point, this should be a successfully cast instant cast spell
-	
-	self:debug(RED.."SPELLCAST_STOP")
-	
-	if self.instant then 
-		self:PredictHeal()
-	end
-end
-
-function Bishop:SPELLCAST_CHANNEL_START()
-	-- This event will fire when we start casting a channeled spell (Tranquility). 
-	-- At this point, we can predict the full amount of the heal.
-	
-	self:debug(GREEN.."SPELLCAST_CHANNEL_START")
-	
-	self:PredictHeal()
-end
-
-function Bishop:CHAT_MSG_SPELL_SELF_BUFF()
-	-- This event will fire when a heal comes across on anyone in the raid or party
-	-- Messages we are interested in: "Your Flash Heal critically heals Cladhaire for 1337."
-	-- SetSpell should have already been called, so we just need to process the prediction
-	
-	local _,_,spell,target,amount = string.find(arg1, BISHOP.MSG_HEAL_PATTERN)
-
-	if not spell or not target or not amount then return end
-	
-	-- Parse the critically out, if it exists
-	local _,_,critName = string.find(spell, BISHOP.MSG_CRIT_PATTERN)
-	
-	if critName then spell = critName end
-	if target == "you" then target = "player" end
-	
-	if not BISHOP.HEALS[spell] then return end
-	
-	local rank = BISHOP.HEALS[spell].lastRank
-
-	-- We need the spell, rank, target and amount in order to pass the heal off
-	
-	self:debug("CHAT_MSG_SPELL_SELF_BUFF: Spell: " .. tostring(spell).. " Amount: " .. tostring(amount) .. " Target: " .. tostring(target))
-	
-	local missing = UnitHealthMax(target) - UnitHealth(target)
-	local actual
-	
-	if (missing - amount) < 0 then
-		actual = missing
-	else
-		actual = amount
-	end
-			
-	self:PredictHeal()
-	
-	self:AddHeal(spell, rank, target, actual)
-	self:ClearData()
-end
-
-function Bishop:CHAT_MSG_SPELL_PERIODIC_SELF_BUFFS()
-	-- This event will fire when a HoT tick goes off on the player.
-	-- Messages will typically look like: "You gain 12 health from Renew."
-	-- Prediction has already been done, we just need to record the heal
-
-	local _,_,actual,spell = string.find(arg1, BISHOP.MSG_HOT_PATTERN_SELF)
-
-	if not actual or not spell then return end
-	if not BISHOP.HEALS[spell] then return end
-
-	self:debug(arg1)
-	
-	local rank = BISHOP.HEALS[spell].lastRank
-	
-	self:AddHeal(spell, rank, "player", actual)
-end
-
-function Bishop:HOT_FRIENDLY()
-	-- This event will fire when a HoT tick goes off on a party member
-	-- Messages will typically look like: "Sagart gains 12 health from your Renew."
-	-- Prediction has already been done, we just need to record the heal
-
-	local _,_,targetName,actual,spell = string.find(arg1, BISHOP.MSG_HOT_PATTERN_OTHER)
-
-	if not targetName or not actual or not spell then return end
-	if not BISHOP.HEALS[spell] then return end
-
-	self:debug(arg1)
+	for spell, items in pairs(rangedData) do
+		for itemid, healing in pairs(items) do
+			if( IsEquippedItem(itemid) ) then
+				equippedBonus.spell = spell
+				equippedBonus.healing = healing
+				break
+			end
+		end
 		
-	local rank = BISHOP.HEALS[spell].lastRank
-	local target = self:GetTargetFromName(targetName)
-	
-	if not target then return end
-	
-	self:AddHeal(spell, rank, target, actual)
-end
-
-
---[[---------------------------------------------------------------------------------
-  Function Hooks
-------------------------------------------------------------------------------------]]
-
-function Bishop:CastSpell(spellId, spellbookTabNum)
-	-- Call the original function
-	self:CallHook("CastSpell", spellId, spellbookTabNum)
-	local name, rank = GetSpellName(spellId, spellbookTabNum)
-
-	if not BISHOP.HEALS[name] then return end
-	_,_,rank = string.find(rank, BISHOP.RANK_PATTERN)
-	
-	self.spellInfo.spell = name
-	self.spellInfo.rank = rank
-	self.spellInfo.instant = BISHOP.HEALS[name].instant
-	
-	if SpellIsTargeting() then
-		self.spellInfo.targeting = true
-	else
-		self.spellInfo.target = UnitName("target")
-		self.predict = true
-
-		if BISHOP.HEALS[name].instant then self:PredictHeal() end
-	end
-end
-
-function Bishop:CastSpellByName(spellName)
-	self:CallHook("CastSpellByName", spellName)
-	local _,_,name,rank = string.find(spellName, BISHOP.NAME_RANK_PATTERN)
-	if not name then return end
-	if not BISHOP.HEALS[name] then return end
-
-	self.spellInfo.spell = name
-	self.spellInfo.rank = rank
-	self.spellInfo.instant = BISHOP.HEALS[name].instant
-
-	if SpellIsTargeting() then
-		self.spellInfo.targeting = true
-	else
-		self.spellInfo.target = UnitName("target")
-		self.predict = true
-
-		if BISHOP.HEALS[name].instant then self:PredictHeal() end
-	end
-end
-
-function Bishop:UseAction(a1, a2, a3)
-	self:CallHook("UseAction", a1, a2, a3)
-	if GetActionText(a1) then return end
-	self.tooltip:SetAction(a1) 
-	local name, rank
-
-	if BishopTooltipTextLeft1:GetText() then 
-		name = BishopTooltipTextLeft1:GetText()
-		if BishopTooltipTextRight1:GetText() then 
-			_,_,rank = string.find(BishopTooltipTextRight1:GetText(), BISHOP.RANK_PATTERN)
+		if( equippedBonus.spell ) then
+			break
 		end
-	else
+	end
+end
+
+-- Scan/cache talents
+function Bishop:ScanPlayerTalents()
+	for tabIndex=1, GetNumTalentTabs() do
+		for i=1, GetNumTalents(tabIndex) do
+			local talentName, _, _, _, pointsSpent = GetTalentInfo(tabIndex, i)	
+			if( not talentName ) then
+				break
+			end
+			
+			playerTalents[talentName] = pointsSpent
+		end
+	end
+end
+
+-- Figure out the total value of the HoT
+function Bishop:CalculateHOTHeal(spellName, rank, spellStack)
+	if( not spellName ) then
+		return 0
+	end
+	
+	rank = tonumber(string.match(rank, "(%d+)"))
+	
+	local healed = 0
+	local spell = spellData[spellName]
+	local addFactor = 1.0
+	local multiFactor = spell[0].duration / 15
+	
+	if( spellStack > spell[0].maxStack ) then
+		spellStack = spell[0].maxStack
+	end
+	
+	-- Apply talent information
+	for talent, data in pairs(talentData) do
+		if( not data.spell or data.spell == spellName ) then
+			if( data.multi ) then
+				multiFactor = multiFactor * ( 1 + ( playerTalents[talent] * data.mod ) )
+			else
+				addFactor = addFactor + ( playerTalents[talent] * data.mod )
+			end
+		end
+	end
+	
+	-- Check if we have any idols/relics/librams increasing our +healing for this
+	local totalHealing = GetSpellBonusHealing()
+	if( equippedBonus.spell == spellName ) then
+		totalHealing = totalHealing + equippedBonus.healing		
+	end
+	
+	-- Now calculate the total amount healed
+	if( spell[0].type == "ddhot" ) then
+		multiFactor = multiFactor * ( 1 - ( spell[0].hotFactor or 0 ) ) * ( spell[0].dotFactor or 1 )
+		healed = addFactor * ( multiFactor * totalHealing + spell[rank].healed )
+
+	elseif( spell[0].type == "hot" ) then
+		healed = addFactor * ( 1 * spell[rank].healed + ( totalHealing * multiFactor ) )
+	end
+	
+	return math.floor((healed * spellStack) + 0.5)
+end
+
+
+function Bishop:CHAT_MSG_SPELL_PERIODIC_FRIENDLYPLAYER_BUFFS(event, msg)
+
+end
+
+function Bishop:CHAT_MSG_SPELL_SELF_BUFF(event, msg)
+end
+
+function Bishop:UNIT_SPELLCAST_SUCCEEDED(event, unit, spell, rank)
+	if( unit ~= "player" ) then
 		return
 	end
-
-	if not name or not rank then return end
-	if not BISHOP.HEALS[name] then return end
 	
-	self.spellInfo.spell = name
-	self.spellInfo.rank = rank
-	self.spellInfo.instant = BISHOP.HEALS[name].instant
- 
-	if SpellIsTargeting() then
-		self.spellInfo.targeting = true
-	else
-		self.spellInfo.target = UnitName("target")
-		self.predict = true
-
-		if BISHOP.HEALS[name].instant then self:PredictHeal() end
+	if( spell == L["Regrowth"] ) then
+		healData[spell] = ( healData[spell] or 0 ) + self:CalculateHOTHeal(spell, rank, 1)
 	end
 end
 
-function Bishop:SpellTargetUnit(unit)
-	self:CallHook("SpellTargetUnit", unit)
-	
-	if self.spellInfo.targeting and not SpellIsTargeting() then
-		self.spellInfo.target = UnitName(unit)
-		self.predict = true
-
-		if BISHOP.HEALS[self.spellInfo.spell].instant then self:PredictHeal() end
-	end
-end
-
-function Bishop:TargetUnit(unit)
-	self:CallHook("TargetUnit", unit)
-
-	if self.spellInfo.targeting and not SpellIsTargeting() then
-		self.spellInfo.target = UnitName(unit)
-		self.predict = true
-
-		if BISHOP.HEALS[self.spellInfo.spell].instant then self:PredictHeal() end
-	end
-end
-
-function Bishop:SpellStopTargeting()
-	self:CallHook("SpellStopTargeting")
-end
-
-function Bishop:CameraOrSelectOrMoveStart()
-	local target= nil
-	
-	if self.spellInfo.spell and UnitName("mouseover") then
-		target = UnitName("mouseover")
-	end
-
-	self:CallHook("CameraOrSelectOrMoveStart")
-
-	if self.spellInfo.targeting and not SpellIsTargeting() then
-		self.spellInfo.target = target
-		self.predict = true
-
-		if BISHOP.HEALS[self.spellInfo.spell].instant then self:PredictHeal() end
-	end
-end
-
---[[---------------------------------------------------------------------------------
-  Processing Functions
-------------------------------------------------------------------------------------]]
-
-function Bishop:PredictHeal()
-	if not self.predict then return end
-	
-	local data = BISHOP.HEALS[self.spellInfo.spell][self.spellInfo.rank]
-	local rootPath = {"healData"}
-	local targetSpellPath = {"healData", self.spellInfo.target}
-	local paths = {rootPath, targetSpellPath}
-	
-	for k,v in paths do
---		self.db:incVal(v, "casts", 1)
+function Bishop:UNIT_SPELLCAST_SENT(event, unit, spell, rank, target)
+	if( unit ~= "player" ) then
+		return
 	end
 	
-	if data.hotAmt then
-		-- We have a HoT amount, so lets predict that first
-		for k,v in paths do 
---			self.db.incVal(v, "amount", data.hotAmt)
+	-- Got to figure it out now for simplicity
+	if( spell == L["Lifebloom"] ) then
+		local i = 1		
+		local spellStack = 1
+		
+		while( true ) do
+			local name, rank, _, stack, totalTime, duration = UnitBuff(target, i)
+			if( not name ) then
+				break
+			end
+
+			if( name == spell and duration ) then
+				spellStack = stack + 1
+				return
+			end
+			
+			i = i + 1
 		end
+
+		healData[spell] = ( healData[spell] or 0 ) + self:CalculateHOTHeal(spell, rank, spellStack)
+	elseif( spell == L["Rejuvenation"] ) then
+		healData[spell] = ( healData[spell] or 0 ) + self:CalculateHOTHeal(spell, rank, spellStack)
 	end
+end
+
+--[[
+"[CHAT_MSG_SPELL_PERIODIC_FRIENDLYPLAYER_BUFFS] Kasath gains 108 health from your Lifebloom.", -- [334]
+"[CHAT_MSG_SPELL_PERIODIC_FRIENDLYPLAYER_BUFFS] Malfice gains 182 health from your Regrowth.", -- [190]
+"[CHAT_MSG_SPELL_SELF_BUFF] Your Swiftmend heals you for 2210.", -- [347]
+"[CHAT_MSG_SPELL_SELF_BUFF] Your Healing Touch heals Malfice for 2449.", -- [192]
+"[CHAT_MSG_SPELL_SELF_BUFF] Your Frenzied Regeneration heals you for 250.", -- [206]
+"[CHAT_MSG_SPELL_SELF_BUFF] Your Lifebloom critically heals you for 1461.", -- [215]
 	
-	if data.min then
-		-- We have a min/max amount, so predict the average between then
-		for k,v in paths do
---			self.db:incVal(v, "amount", (data.min + data.max) / 2)
-		end
-	end
-	
-	ace:print(HEADER.." Predicted " .. self.spellInfo.spell .. " Rank " .. self.spellInfo.rank .. " on " .. tostring(self.spellInfo.target)) 
-	
-	-- Change predict so we don't try this again (Chain Heal, PoF, etc)
-	self.predict = nil
-	self.instant = nil
-	self.targeting = nil
-
-	self:ClearData()	
-end
-
-function Bishop:SetSpell(spell, rank, target)
-end
-
-function Bishop:AddHeal(spell, rank, target, actual)
-	ace:print(HEADER.." AddHeal(" ..tostring(spell)..", "..tostring(rank)..", "..tostring(target)..", "..tostring(actual))
-end
-
-function Bishop:ClearData()
-	local s = self.spellInfo
-	self.lastSpell = s.spell
-	self.lastRank = s.rank
-
-	s.spell = nil
-	s.targeting = nil
-	s.instant = nil
-	s.rank = 9
-end
-
-function Bishop:GetTargetFromName(name)
-end
-
-function Bishop:OnEnter()
-end
-
---[[ Data is saved in the following format:
-
-healData = {
-	["Sagart"] = {
-		class 	= "Priest",	
-		["Flash Heal"] = {
-			["Rank 6"] = {
-				actual 	= 0,
-				amount 	= 0,
-				casts 	= 0,
-				crits	= 0,
-			}
-		},
-		["Greater Heal"] = {		
-			["Rank 4"] = {
-				actual 	= 0,
-				amount 	= 0,
-				casts 	= 0,
-				crits	= 0,
-			},
-		},
-	},
-}
-
---]]
-
-Bishop:RegisterForLoad()
+HEALEDCRITOTHER = "%s critically heals %s for %d.";
+HEALEDCRITOTHEROTHER = "%s's %s critically heals %s for %d.";
+HEALEDCRITOTHERSELF = "%s's %s critically heals you for %d.";
+HEALEDCRITSELF = "%s critically heals you for %d.";
+HEALEDCRITSELFOTHER = "Your %s critically heals %s for %d.";
+HEALEDCRITSELFSELF = "Your %s critically heals you for %d.";
+HEALEDOTHER = "%s heals %s for %d.";
+HEALEDOTHEROTHER = "%s's %s heals %s for %d.";
+HEALEDOTHERSELF = "%s's %s heals you for %d.";
+HEALEDSELF = "%s's %s heals you for %d.";
+HEALEDSELFOTHER = "Your %s heals %s for %d.";
+HEALEDSELFSELF = "Your %s heals you for %d.";
+]]
