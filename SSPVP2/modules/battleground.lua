951744f105a082eb0ca294636817cf83374abf91
shadowed.wow <shadowed.wow@7083c368-1f31-0410-96f0-4f2793cbc738>
2007-11-22 06:55:05 +0000

    * Base Arathi Basin support is in, just does timers currently * Implemented all the arena features, inspect/player points, (L) next to name, float game played and /arena for conversion * Added all the reformat/battleground things like auto appending server, blocking raid leave/join, timer to start, auto release * Added all the score modifications, coloring names, hiding class icon and enemy/friendly levels (Alliance/Horde break down not in yet) * Overlays should fully work now, changed so instead of just clicking the overlay to active the OnClick you now have to hold a modifier key down to prevent accidental clicks

---
+++ SSPVP2/modules/battleground.lua
@@ -0,0 +1,104 @@
+local BF = SSPVP:NewModule("Battlefield", "AceEvent-3.0")
+BF.activeIn = "bf"
+
+local L = SSPVPLocals
+
+function BF:OnEnable()
+	self.defaults = {
+		profile = {
+			release = true,
+			whispers = true,
+			soulstone = false,
+		},
+	}
+	
+	self.db = SSPVP.db:RegisterNamespace("battlefield", self.defaults)
+end
+
+function BF:EnableModule(abbrev)
+	self:RegisterEvent("CHAT_MSG_BG_SYSTEM_NEUTRAL")
+	
+
+	-- May not want to auto release in arenas incase a team mates going to try
+	-- and ressurect you
+	if( abbrev ~= "arena" ) then
+		self:RegisterEvent("PLAYER_DEAD")
+	end
+end
+function BF:DisableModule()
+	SSOverlay:RemoveRow("start")
+	self:UnregisterAllEvents()
+end
+
+-- Start timers
+function BF:CHAT_MSG_BG_SYSTEM_NEUTRAL(event, msg)
+	if( string.match(msg, L["2 minute"]) ) then
+		SSOverlay:RegisterTimer("start", "timer", L["Starting: %s"], 120)
+	elseif( string.match(msg, L["1 minute"]) or string.match(msg, L["One minute"]) ) then
+		SSOverlay:RegisterTimer("start", "timer", L["Starting: %s"], 60)
+	elseif( string.match(msg, L["30 seconds"]) or string.match(msg, L["Thirty seconds"]) ) then
+		SSOverlay:RegisterTimer("start", "timer", L["Starting: %s"], 30)
+	elseif( string.match(msg, L["Fifteen seconds"]) ) then
+		SSOverlay:RegisterTimer("start", "timer", L["Starting: %s"], 15)
+	end
+end
+
+-- Auto release
+function BF:PLAYER_DEAD()
+	if( self.db.profile.release ) then
+		-- No soul stone, release
+		if( not HasSoulstone() ) then
+			StaticPopupDialogs["DEATH"].text = L["Releasing..."]
+			RepopMe()	
+		
+		-- Soul stone active, and we should auto use it
+		elseif( HasSoulstone() and self.db.profile.soulstone ) then
+			StaticPopupDialogs["DEATH"].text = string.format(L["Using %s..."], HasSoulstone())
+			UseSoulstone()		
+		
+		-- Soul stone active, don't auto release
+		else
+			StaticPopupDialogs["DEATH"].text = HasSoulstone()	
+		end
+	else
+		StaticPopupDialogs["DEATH"].text = TEXT(DEATH_RELEASE_TIMER)
+	end
+end
+
+-- Block annoying raid join/leaves inside battlegrounds
+local Orig_ChatFrame_SystemEventHandler = ChatFrame_SystemEventHandler
+function ChatFrame_SystemEventHandler(event, ...)
+	if( BF.isActive and arg1 and string.match(arg1, L["the raid group.$"]) ) then
+		return true
+	end
+	
+	return Orig_ChatFrame_SystemEventHandler(event, ...)
+end
+
+-- Auto append server name
+local Orig_SendChatMessage = SendChatMessage
+function SendChatMessage(text, type, language, target, ...)
+	-- See if we should try and find a match to the one we gave
+	if( BF.isActive and type == "WHISPER" and target and BF.db.profile.whispers and not string.match(target, "-") ) then
+		local results = 0
+		local player
+
+		-- Scan scores find match(es)
+		for i=1, GetNumBattlefieldScores() do
+			local name = GetBattlefieldScore(i)
+			
+			-- Make sure they're from another server
+			if( string.match(string.lower(name), "^" .. string.lower(target)) ) then
+				player = name
+				results = results + 1
+			end
+		end
+		
+		-- If we only found one match, set the new name, otherwise discard it
+		if( results == 1 ) then
+			target = player
+		end
+	end
+	
+	return Orig_SendChatMessage(text, type, language, target, ...)
+end
---
+++ SSPVP2/modules/overlay.lua
@@ -0,0 +1,406 @@
+SSOverlay = SSPVP:NewModule("Overlay")
+
+local L = SSPVPLocals
+
+local CREATED_ROWS = 0
+local MAX_ROWS = 25
+local ADDED_ENTRIES = 0
+local longestText = 0
+local resortRows
+
+local rows = {}
+local catCount = {}
+local categories = {
+	["faction"] = { label = L["Faction Balance"], order = 0 },
+	["timer"] = { label = L["Timers"], order = 20 },
+	["match"] = { label = L["Match Info"], order = 30 },
+	["base"] = { label = L["Bases to win"], order = 40 },
+}
+
+function SSOverlay:OnEnable()
+	self.defaults = {
+		profile = {
+			locked = false,
+			x = 300,
+			y = 600,
+			opacity = 1.0,
+			background = { r = 0, g = 0, b = 0 },
+			border = { r = 0.75, g = 0.75, b = 0.75 },
+			textColor = { r = 1, g = 1, b = 1 },
+			categoryColor = { r = 0.75, g = 0.75, b = 0.75 },
+			scale = 1.0,
+		},
+	}
+	
+	self.db = SSPVP.db:RegisterNamespace("overlay", self.defaults)
+end
+
+local function onClick(self)
+	-- So you won't accidentally click the overlay, make sure we have an on click too
+
+	if( not IsModifierKeyDown() or not rows[self.dataID].func ) then
+
+		return
+	end
+	
+	local row = rows[self.dataID]
+	-- Trigger it
+	if( row.handler ) then
+		row.handler[row.func](row.handler, unpack(row.args))
+	elseif( type(row.func) == "string" ) then
+		getglobal(row.func)(unpack(row.args))
+	elseif( type(row.func) == "function" ) then
+		row.func(unpack(row.args))
+	end
+end
+
+local function onUpdate(self)
+	local time = GetTime()
+	local row = rows[self.dataID]
+	
+	if( row.type == "up" ) then
+		row.seconds = row.seconds + (time - row.lastUpdate)
+	elseif( row.type == "down" ) then
+
+		row.seconds = row.seconds - (time - row.lastUpdate)
+	end
+	
+	row.lastUpdate = time
+	
+	if( floor(row.seconds) <= 0 ) then
+		SSOverlay:RemoveRow(row.id)
+	else
+		self.text:SetFormattedText(row.text, SecondsToTime(row.seconds))
+	end
+end
+
+-- Update display
+local function sortOverlay(a, b)
+	if( not a ) then
+		return true
+	elseif( not b ) then
+		return false
+	end
+	
+	if( a.sortID ~= b.sortID ) then
+		return a.sortID < b.sortID
+	end
+
+	return a.addOrder < b.addOrder
+end
+
+function SSOverlay:UpdateCategoryText()
+	-- Figure out total unique categories we're showing
+	local activeCats = 0
+	for _, total in pairs(catCount) do
+		if( total > 0 ) then
+			activeCats = activeCats + 1
+		end
+	end
+			
+	-- Now add category texts as required
+	for name, total in pairs(catCount) do
+		if( activeCats > 1 and total > 0 ) then
+			self:RegisterRow("catText", "cat" .. name, name, categories[name].label, nil, nil, 1)
+		else
+			self:RemoveRow("cat" .. name)
+		end
+	end
+end
+
+function SSOverlay:UpdateOverlay()
+	local totalRows = #(rows)
+	if( totalRows == 0 ) then
+		if( self.frame ) then
+
+			self.frame:Hide()
+
+		end
+		return
+	end
+	
+	if( not self.frame ) then
+		self:CreateFrame()
+	end
+	
+
+	if( resortRows ) then
+		table.sort(rows, sortOverlay)
+		resortRows = nil
+	end
+	
+	
+	for id, data in pairs(rows) do
+		if( id > MAX_ROWS ) then
+			break
+		end
+		
+		local row = self.rows[id]
+		if( not row ) then
+			row = self:CreateRow()
+		end
+		
+		-- Text rows just need static text no fancy stuff
+		-- timers and elapsed rows actually need an OnUpdate
+		if( data.type == "text" or data.type == "catText" ) then
+			row.text:SetText(data.text)
+			row:SetScript("OnUpdate", nil)
+		elseif( data.type == "up" or data.type == "down" ) then
+			row.text:SetFormattedText(data.text, SecondsToTime(data.seconds))
+			row:SetScript("OnUpdate", onUpdate)
+		end
+		
+		if( data.color ) then
+			row.text:SetTextColor(data.color.r, data.color.g, data.color.b)
+		elseif( data.type == "catText" ) then
+			row.text:SetTextColor(self.db.profile.categoryColor.r, self.db.profile.categoryColor.g, self.db.profile.categoryColor.b)
+		else
+			row.text:SetTextColor(self.db.profile.textColor.r, self.db.profile.textColor.g, self.db.profile.textColor.b)
+		end
+		
+		row.dataID = id
+		row:Show()
+		
+		if( longestText < row.text:GetStringWidth() ) then
+			longestText = row.text:GetStringWidth() + 20
+		end
+	end
+	
+	-- Hide anything unused, and adjust the row width to match the overlay
+	for i=1, CREATED_ROWS do
+		if( i > totalRows ) then
+			self.rows[i].dataID = nil
+			self.rows[i]:Hide()
+		else
+			self.rows[i]:SetWidth(longestText + 15)
+		end
+	end
+	
+	-- Resize
+	self.frame:SetHeight(min(MAX_ROWS, totalRows) * (self.rows[1].text:GetHeight() + 2) + 9)
+	self.frame:SetWidth(longestText)
+	self.frame:Show()
+end
+
+-- Remove an entry by id or category
+function SSOverlay:RemoveAll()
+	for i=#(rows), 1, -1 do
+		table.remove(rows, i)
+	end
+	
+	for cat in pairs(catCount) do
+		catCount[cat] = nil
+	end
+	
+	if( self.frame ) then
+		for i=1, CREATED_ROWS do
+			self.rows[i]:Hide()
+		end
+		
+		self.frame:Hide()
+	end
+end
+
+function SSOverlay:RemoveRow(id)
+	for i=#(rows), 1, -1 do
+		local row = rows[i]
+		if( row and row.id == id ) then
+			longestText = 0
+			table.remove(rows, i)
+			
+			if( row.type ~= "catText" ) then
+				catCount[row.category] = catCount[row.category] - 1
+				self:UpdateCategoryText()
+			end
+
+			
+			self:UpdateOverlay()
+
+		end
+	end
+end
+
+function SSOverlay:RemoveCategory(category)
+	local updated
+	for i=#(rows), 1, -1 do
+		if( rows[i].category == category ) then
+			table.remove(rows, i)
+			updated = true
+		end
+	end
+	
+	if( updated ) then
+		longestText = 0
+		catCount[category] = nil
+		
+		self:UpdateCategoryText()
+
+		self:UpdateOverlay()
+	end
+end
+
+-- Adding new rows
+function SSOverlay:RegisterText(id, category, text, color)
+	self:RegisterRow("text", id, category, text, color, nil, 4)
+end
+
+function SSOverlay:RegisterTimer(id, category, text, seconds, color)
+	self:RegisterRow("down", id, category, text, color, seconds, 2)
+end
+
+function SSOverlay:RegisterElapsed(id, category, text, seconds, color)
+	self:RegisterRow("up", id, category, text, color, seconds, 3)
+end
+
+-- Generic register, only used internally
+function SSOverlay:RegisterRow(type, id, category, text, color, seconds, priority)
+	local row
+	local newRow
+	-- Grab an existing entry if we haven't deleted them yet
+	for _, data in pairs(rows) do
+		if( data.id == id ) then
+			row = data
+			break
+		end
+	end
+	
+	if( not row ) then
+		row = {}
+		newRow = true
+	end
+	
+	row.type = type
+	row.id = id
+	row.category = category
+	row.text = text
+	row.color = color
+	row.category = category
+	row.addOrder = row.addOrder or ADDED_ENTRIES
+	row.sortID = categories[category].order + priority
+	
+	-- Set start time and last update for timers
+	if( type == "up" or type == "down" ) then
+		row.seconds = seconds
+		row.lastUpdate = GetTime()
+	end	
+
+	-- New row time
+	if( newRow ) then
+		ADDED_ENTRIES = ADDED_ENTRIES + 1
+		resortRows = true
+		table.insert(rows, row)
+		
+		-- Infinite recusion is bad
+		if( row.type ~= "catText" ) then
+			catCount[category] = (catCount[category] or 0 ) + 1
+			self:UpdateCategoryText()
+		end
+
+	end
+	
+	self:UpdateOverlay()
+end
+
+-- Associates something to run when we click on a row in the overlay
+local noArgs = {}
+function SSOverlay:RegisterOnClick(id, handler, func, ...)
+	local row
+	for _, data in pairs(rows) do
+		if( data.id == id ) then
+
+			row = data
+			break
+		end
+	end
+	
+
+	if( not row ) then
+		return
+	end
+	
+	if( type(handler) == "function" or type(handler) == "string" ) then
+		row.func = handler
+	elseif( type(handler) == "table" and type(func) == "string" ) then
+		row.handler = handler
+		row.func = func
+	end
+	
+	if( select("#", ...) > 0 ) then
+		row.args = { ... }
+	else
+		row.args = noArgs
+	end
+end
+
+-- Create container frame
+function SSOverlay:CreateFrame()
+	self.rows = {}
+
+	-- Setup the overlay frame
+	self.frame = CreateFrame("Frame", nil, UIParent)
+	self.frame:RegisterForDrag("LeftButton")
+	self.frame:SetScale(self.db.profile.scale)
+	self.frame:SetClampedToScreen(true)
+	self.frame:SetMovable(true)
+	self.frame:EnableMouse(true)
+	self.frame:SetFrameStrata("BACKGROUND")
+	self.frame:SetPoint("TOPLEFT", UIParent, "BOTTOMLEFT", self.db.profile.x, self.db.profile.y)
+
+	self.frame:SetScript("OnMouseUp", function(self)
+		if( self.isMoving ) then
+			self:StopMovingOrSizing()
+
+			SSOverlay.db.profile.x = self:GetLeft()
+			SSOverlay.db.profile.y = self:GetTop()
+		end
+	end)
+
+	self.frame:SetScript("OnMouseDown", function(self)
+		if( not SSOverlay.db.profile.locked ) then
+			self.isMoving = true
+			self:StartMoving()
+		end
+	end)
+
+	self.frame:SetBackdrop({bgFile = "Interface\\Tooltips\\UI-Tooltip-Background",
+				edgeFile = "Interface\\Tooltips\\UI-Tooltip-Border",
+				tile = true,
+				tileSize = 9,
+				edgeSize = 9,
+				insets = { left = 2, right = 2, top = 2, bottom = 2 }})	
+
+	self.frame:SetBackdropColor(self.db.profile.background.r, self.db.profile.background.g, self.db.profile.background.b, self.db.profile.opacity)
+	self.frame:SetBackdropBorderColor(self.db.profile.border.r, self.db.profile.border.g, self.db.profile.border.b, self.db.profile.opacity)
+end
+
+-- Create a new row
+function SSOverlay:CreateRow()
+	if( CREATED_ROWS >= MAX_ROWS or not self.frame ) then
+		return
+	end
+
+	CREATED_ROWS = CREATED_ROWS + 1
+
+	local row = CreateFrame("Frame", nil, self.frame)
+	row:EnableMouse(self.db.profile.locked)	
+	row:SetScript("OnMouseUp", onClick)
+	row:SetFrameStrata("LOW")
+	row:SetHeight(13)
+	row:SetWidth(250)
+
+	local text = row:CreateFontString(nil, "BACKGROUND")
+	text:SetJustifyH("LEFT")
+	text:SetFontObject(GameFontNormalSmall)
+	text:SetPoint("TOPLEFT", row, "TOPLEFT", 0, 0)
+	row.text = text
+	
+	if( CREATED_ROWS > 1 ) then
+		row:SetPoint("TOPLEFT", self.rows[CREATED_ROWS - 1], "TOPLEFT", 0, -12)
+	else
+		row:SetPoint("TOPLEFT", self.frame, "TOPLEFT", 5, -5)
+	end
+
+	self.rows[CREATED_ROWS] = row
+	
+	return row
+end
