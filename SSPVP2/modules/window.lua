95ce00adc0b034123f3c2412aea2a7cd50cb6e48
shadowed.wow <shadowed.wow@7083c368-1f31-0410-96f0-4f2793cbc738>
2007-12-22 08:15:40 +0000

    * Fixed ready check not working * Fixed syncing being off by a second (3 second sync = 2, 1, queue instead of 3, 2, 1, queue) * Fixed total player stats

--- SSPVP2/modules/window.lua
+++ SSPVP2/modules/window.lua
@@ -1,11 +1,14 @@
 local Window = SSPVP:NewModule("Window", "AceEvent-3.0")
 local L = SSPVPLocals
+
 local entryDialog
+local lastStatus = {}
 
 function Window:OnInitialize()
 	self.defaults = {
 		profile = {
-			enabled = false
+			enabled = false,
+			remind = false,
 		},
 	}
 	
@@ -19,3 +22,54 @@ function Window:OnInitialize()
 		StaticPopupDialogs["CONFIRM_BATTLEFIELD_ENTRY"] = nil
 	end
 end
+
+function Window:OnEnable()
+	if( self.db.profile.enabled ) then
+		self:RegisterEvent("UPDATE_BATTLEFIELD_STATUS")
+	end
+end
+
+function Window:Reload()
+	self:UnregisterAllEvents()
+	self:OnEnable()
+	
+	-- Restore the original entry
+	if( self.db.profile.enabled ) then
+		if( StaticPopupDialogs["CONFIRM_BATTLEFIELD_ENTRY"] and not entryDialog ) then
+			entryDialog = StaticPopupDialogs["CONFIRM_BATTLEFIELD_ENTRY"]
+			StaticPopupDialogs["CONFIRM_BATTLEFIELD_ENTRY"] = nil
+		end
+	else
+		if( not StaticPopupDialogs["CONFIRM_BATTLEFIELD_ENTRY"] and entryDialog ) then
+			StaticPopupDialogs["CONFIRM_BATTLEFIELD_ENTRY"] = entryDialog
+			entryDialog = nil
+		end
+	end
+end
+
+function Window:UPDATE_BATTLEFIELD_STATUS()
+	for i=1, MAX_BATTLEFIELD_QUEUES do
+		local status, map, instanceID, _, _, teamSize, isRegistered = GetBattlefieldStatus(i)
+		
+		if( status ~= lastStatus[i] ) then
+			if( status == "confirm" ) then
+				--StaticPopup_Show("
+			end
+		end
+		
+		lastStatus[i] = status	
+	end
+end
+
+StaticPopupDialogs["CONFIRM_NEW_BFENTRY"] = {
+	text = L["You can now enter %s and have %s left."],
+	button1 = ENTER_BATTLE,
+	button2 = HIDE,
+	OnAccept = function(data)
+		AcceptBattlefieldPort(data, 1)
+	end,
+	timeout = 0,
+	whileDead = 1,
+	hideOnEscape = 1,
+	multiple = 1
+};
