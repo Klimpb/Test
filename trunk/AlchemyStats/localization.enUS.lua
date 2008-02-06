50edf1bfb5a0fb7fe2fcba0643ba455f7be7dd80
shadowed.wow <shadowed.wow@7083c368-1f31-0410-96f0-4f2793cbc738>
2008-02-06 00:48:12 +0000

    * Now With More Ace3(TM), game restart required for this version * Cleaned up the code to actually make more sense * Will save if something was created with the mastery required so we know when to use no proc stats * Removed /alchemy stat, type /alchemy now to pull up the GUI that lists all the stats * All recorded data will be wiped for this version, but not again

--- trunk/AlchemyStats/localization.enUS.lua
+++ trunk/AlchemyStats/localization.enUS.lua
@@ -1,9 +1,23 @@
 AlchemyStatLocals = {
 	["Alchemy"] = "Alchemy",
+	["Alchemy Stats"] = "Alchemy Stats",
 	["Elixir Master"] = "Elixir Master",
 	["Potion Master"] = "Potion Master",
 	["Transmutation Master"] = "Transmutation Master",
 	
-	["/alchemy stat - Shows total types of items created through alchemy"] = "/alchemy stat - Shows total types of items created through alchemy",
-	["/alchemy <elixir/flask/potion/elemental> - Show total created/item stats of the passed item type"] = "/alchemy <elixir/flask/potion/elemental> - Show total created/item stats of the passed item type",
+	["Total Made"] = "Total Made",
+	["Total Procs"] = "Total Procs",
+	
+
+	["Item"] = "Item",
+	["Created"] = "Created",
+	["Procs"] = "Procs",
+	["One"] = "One",
+	["Two"] = "Two",
+	["Three"] = "Three",
+	["Four"] = "Four",
+	["One Proc"] = "One Proc",
+	["Two Proc"] = "Two Proc",
+	["Three Proc"] = "Three Proc",
+	["Four Proc"] = "Four Proc",
 }
