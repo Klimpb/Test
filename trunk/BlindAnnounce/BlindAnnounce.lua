0155044683492bd6f2ee2217200838f6971de00d
shadowed.wow <shadowed.wow@7083c368-1f31-0410-96f0-4f2793cbc738>
2008-04-03 07:59:54 +0000

    * Things

--- trunk/BlindAnnounce/BlindAnnounce.lua
+++ trunk/BlindAnnounce/BlindAnnounce.lua
@@ -1,20 +1,42 @@
-local ARENAS_ONLY = true
+local ARENAS_ONLY = false
+local CHANNEL = "PARTY"
+local failMessage = "%s FAILED (%s)"
+local spells = {
+	["Sap"] = "SAPPED %s",
+	["Blind"] = "BLINDED %s",
+}
+local targets = {}
 
-local blindTarget
 local cast = CreateFrame("Frame")
 cast:RegisterEvent("UNIT_SPELLCAST_SENT")
 cast:RegisterEvent("UNIT_SPELLCAST_SUCCEEDED")
+cast:RegisterEvent("COMBAT_LOG_EVENT_UNFILTERED")
 cast:SetScript("OnEvent", function(self, event, ...)
-	if( event == "UNIT_SPELLCAST_SENT" ) then
-		local unit, spellName, spellRank, targetName = ...
-		if( unit == "player" and spellName == "Blind" ) then
-			blindTarget = targetName
-		end
-	elseif( event == "UNIT_SPELLCAST_SUCCEEDED" and blindTarget ) then
-		local unit, spellName, spellRank = ...
-		if( unit == "player" and spellName == "Blind" ) then
+	-- Check for spell misses
+	if( event == "COMBAT_LOG_EVENT_UNFILTERED" and select(2, ...) == "SPELL_MISSED" ) then
+		local timestamp, eventType, sourceGUID, sourceName, sourceFlags, destGUID, destName, destFlags, spellID, spellName, spellSchool, missType = ...
+		if( spells[spellName] ) then
 			if( GetNumPartyMembers() > 0 and ( (ARENAS_ONLY and select(2, IsInInstance()) == "arena") or not ARENAS_ONLY )  ) then
-				SendChatMessage("BLINDED " .. blindTarget, "PARTY")
+				SendChatMessage(string.format(failMessage, spellName, string.upper(getglobal("ACTION_SPELL_MISSED_" .. missType))), CHANNEL)
+			end
+		end
+	
+
+	-- Sent to server
+	elseif( event == "UNIT_SPELLCAST_SENT" ) then
+		local unit, spellName, spellRank, targetName = ...
+		if( unit == "player" and spells[spellName] ) then
+			targets[spellName] = targetName
+		end
+	
+
+	-- Spell caste
+
+	elseif( event == "UNIT_SPELLCAST_SUCCEEDED" ) then
+		local unit, spellName, spellRank = ...
+		if( unit == "player" and spells[spellName] and targets[spellName] ) then
+			if( GetNumPartyMembers() > 0 and ( (ARENAS_ONLY and select(2, IsInInstance()) == "arena") or not ARENAS_ONLY )  ) then
+				SendChatMessage(string.format(spells[spellName], targets[spellName]), CHANNEL)
 			end
 		end
 	end
