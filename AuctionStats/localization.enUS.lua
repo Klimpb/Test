cfb0998b1e12791a3460ee882337d5b4b6950215
shadowed.wow <shadowed.wow@7083c368-1f31-0410-96f0-4f2793cbc738>
2008-06-01 19:23:36 +0000

    * Added average made/spent/profit to the tooltips * Added validation to the character drop down, if it doesn't have the completedAuctions and completedBids/Buyouts fields it won't be listed * Increased the frame size so you can see 31 days worth of data, and 26 rows worth of item data * Fixed a bug where if you had more then one months worth of data it would calculate the total month stats wrong * Fixed a positioning bug where if you had more then a months worth of data the next month would be off to the right

--- AuctionStats/localization.enUS.lua
+++ AuctionStats/localization.enUS.lua
@@ -2,8 +2,10 @@ AuctionStatLocals = {
 	["Auction Stats"] = "Auction Stats",
 	["Choose which characters data should be gathered from, from BeanCounter."] = "Choose which characters data should be gathered from, from BeanCounter.",
 	
-	["Made: |cffffffff%s|rg\nSpent: |cffffffff%s|rg\nProfit: |cffffffff%s|rg"] = "Made: |cffffffff%s|rg\nSpent: |cffffffff%s|rg\nProfit: |cffffffff%s|rg",
+	["|cffffffffToday|r\nMade: |cffffffff%s|rg\nSpent: |cffffffff%s|rg\nProfit: |cffffffff%s|rg\n\n|cffffffffAuctions|r\nCompleted: |cffffffff%s|r\nWon: |cffffffff%s|r"] = "|cffffffffToday|r\nMade: |cffffffff%s|rg\nSpent: |cffffffff%s|rg\nProfit: |cffffffff%s|rg\n\n|cffffffffAuctions|r\nCompleted: |cffffffff%s|r\nWon: |cffffffff%s|r",
 
+	["|cffffffffMonth|r\nMade: |cffffffff%s|rg\nSpent: |cffffffff%s|rg\nProfit: |cffffffff%s|rg\n\n|cffffffffPer Day|r\nMade: |cffffffff%s|rg\nSpent: |cffffffff%s|rg\nProfit: |cffffffff%s|rg"] = "|cffffffffMonth|r\nMade: |cffffffff%s|rg\nSpent: |cffffffff%s|rg\nProfit: |cffffffff%s|rg\n\n|cffffffffPer Day|r\nMade: |cffffffff%s|rg\nSpent: |cffffffff%s|rg\nProfit: |cffffffff%s|rg",
+	
 	["Auctions Completed: %d\nAverage Price: |cffffffff%s|rg"] = "Auctions Completed: %d\nAverage Price: |cffffffff%s|rg",
 	["Auctions Won: %d\nAverage Price: |cffffffff%s|rg"] = "Auctions Won: %d\nAverage Price: |cffffffff%s|rg",
 	
