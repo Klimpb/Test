8fcf83ac6edc2a69a72d09643f3033d892130ebc
shadowed.wow <shadowed.wow@7083c368-1f31-0410-96f0-4f2793cbc738>
2008-06-06 04:24:59 +0000

    * Initial Import: PartyCCTracker/ * Tracks the time left on party timers, uses syncing to make sure their 99% accurate meaning if you want CC timers for someone they'd have to use this and run it in silent mode * Slash command /partycc or /pcc

---
+++ trunk/PartyCCTracker/localization.enUS.lua
@@ -0,0 +1,45 @@
+PartyCCLocals = {
+	-- Cmd
+	["Party CC Tracker slash commands"] = "Party CC Tracker slash commands",
+
+	["- clear - Clears all running timers."] = "- clear - Clears all running timers.",
+	["- ui - Opens the configuration."] = "- ui - Opens the configuration.",
+	["- test - Shows test timers."] = "- test - Shows test timers.",
+
+	-- GUI
+	["None"] = "None",
+	["General"] = "General",
+		
+	["Show anchor"] = "Show anchor",
+	["Display timer anchors for moving around."] = "Display timer anchor for moving around.",
+	
+	["Only show trigger name in bars"] = "Only show trigger name in bars",
+	
+	["Display scale"] = "Display scale",
+	["How big the actual timers should be."] = "How big the actual timers should be.",
+		
+	["Bar width"] = "Bar width",
+	["Bar texture"] = "Bar texture",
+
+	["Redirect bars to group"] = "Redirect bars to group",
+	["Group name to redirect bars to, this lets you show Party CC Tracker timers under another addons bar group. Requires the bars to be created using GTB."] = "Group name to redirect bars to, this lets you show Party CC Tracker timers under another addons bar group. Requires the bars to be created using GTB.",
+
+	["Enable Party CC Tracker inside"] = "Enable Party CC Tracker inside",
+
+	["Allows you to set what scenario's Party CC Tracker should be enabled inside."] = "Allows you to set what scenario's Party CC Tracker should be enabled inside.",
+	
+	["Silent mode"] = "Silent mode",
+	["Disables all timers, and just syncs your CCs to other players."] = "Disables all timers, and just syncs your CCs to other players.",
+	
+	["Spells"] = "Spells",
+	["List"] = "List",
+	["Lets you choose which spells should be shown if a party member uses them."] = "Lets you choose which spells should be shown if a party member uses them.",
+	
+	["Enable timers for %s"] = "Enable timers for %s",
+	
+	["Everywhere else"] = "Everywhere else",
+	["Battlegrounds"] = "Battlegrounds",
+	["Arenas"] = "Arenas",
+	["Raid instances"] = "Raid instances",
+	["Party instances"] = "Party instances",
+}
