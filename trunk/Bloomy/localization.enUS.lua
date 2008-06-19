e0b5ac1b854b261a60121c223f221709927c6b97
shadowed.wow <shadowed.wow@7083c368-1f31-0410-96f0-4f2793cbc738>
2008-06-19 05:00:50 +0000

    * Initial Import: Bloomy * Tries to make it easier to roll lifeblooms by auto setting macros using a list if you have multiple tanks and such, includes a small timer window that can be opened by typing /bloomy toggle * Work in progress, ect, ect * Slash command: /bloomy

---
+++ trunk/Bloomy/localization.enUS.lua
@@ -0,0 +1,16 @@
+BloomyLocals = {
+	["Added \"%s\" to Bloomy ID \"%s\""] = "Added \"%s\" to Bloomy ID \"%s\"",
+	["Removed \"%s\" from Bloomy ID \"%s\""] = "Removed \"%s\" from Bloomy ID \"%s\"",
+	["Reset all Bloomy macros."] = "Reset all Bloomy macros.",
+	["Cannot remove \"%s\" from id \"%s\", no Bloomy macro exists with that ID yet."] = "Cannot remove \"%s\" from id \"%s\", no Bloomy macro exists with that ID yet.",
+	
+	["In order for a macro to be recognized by Bloomy, it has to have an identifier in it."] = "In order for a macro to be recognized by Bloomy, it has to have an identifier in it.",
+	["Add #bloomy <id> <spell name> to the macro and it'll automatically be edited with the person to cast the passed spell on."] = "Add #bloomy <id> <spell name> to the macro and it'll automatically be edited with the person to cast the passed spell on.",
+
+	["Bloomy slash commands"] = "Bloomy slash commands",
+	["/bloomy toggle - Toggles the Bloomy timer frame"] = "/bloomy toggle - Toggles the Bloomy timer frame",
+	["/bloomy add <id> <name> - Adds a group member to the passed id which will have the duration of the HoT left shown in the action button."] = "/bloomy add <id> <name> - Adds a group member to the passed id which will have the duration of the HoT left shown in the action button.",
+	["/bloomy remove <id> <name> - Removes the given group member from the passed id and hots will no longer be shown for them in the action button."] = "/bloomy remove <id> <name> - Removes the given group member from the passed id and hots will no longer be shown for them in the action button.",
+	["/bloomy reset - Removed all assigned group members from Bloomy macros."] = "/bloomy reset - Removed all assigned group members from Bloomy macros.",
+	["/bloomy help - Shows information on how to create a Bloomy macro."] = "/bloomy help - Shows information on how to create a Bloomy macro.",
+}
