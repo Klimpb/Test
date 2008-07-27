fb51330713b3ed8335d2029528875525a571a1d7
shadowed.wow <shadowed.wow@7083c368-1f31-0410-96f0-4f2793cbc738>
2008-07-27 03:41:24 +0000

    * Initial Import: AfflictedTalents/ * Modifies Afflicted timers based on the enemies talents using TalentGuess-1.0, not sure how I want to take this yet so commiting and ignoring for now.

---
+++ AfflictedTalents/lib/update.bat
@@ -0,0 +1,7 @@
+svn export http://dev.wowace.com/wowace/trunk/Ace3/AceAddon-3.0 --force
+svn export http://dev.wowace.com/wowace/trunk/Ace3/AceDB-3.0 --force
+svn export http://dev.wowace.com/wowace/trunk/Ace3/AceEvent-3.0 --force
+svn export http://dev.wowace.com/wowace/trunk/Ace3/AceHook-3.0 --force
+svn export http://dev.wowace.com/wowace/trunk/Ace3/CallbackHandler-1.0 --force
+svn export http://shadowed-wow.googlecode.com/svn/trunk/TalentGuess-1.0 --force
+@PAUSE
