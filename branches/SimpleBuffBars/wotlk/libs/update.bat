2ac04596b554cfa03e7137069c5667180b3cd8e5
shadowed.wow <shadowed.wow@7083c368-1f31-0410-96f0-4f2793cbc738>
2008-08-11 20:06:52 +0000

    * Initial Import: SimpleBuffBars/ (WOTLK) * Nothing really changed, just a quick fix to get it working with WoTLK, I'll probably update the current SBB so it'll mostly work on both versions

---
+++ branches/SimpleBuffBars/wotlk/libs/update.bat
@@ -0,0 +1,9 @@
+svn export http://dev.wowace.com/wowace/branches/Ace3/WotLK/AceDB-3.0 --force
+svn export http://dev.wowace.com/wowace/branches/Ace3/WotLK/AceAddon-3.0 --force
+svn export http://dev.wowace.com/wowace/branches/Ace3/WotLK/AceEvent-3.0 --force
+svn export http://dev.wowace.com/wowace/branches/Ace3/WotLK/AceDBOptions-3.0 --force
+svn export http://dev.wowace.com/wowace/branches/Ace3/WotLK/AceGUI-3.0 --force
+svn export http://dev.wowace.com/wowace/branches/Ace3/WotLK/AceConfig-3.0 --force
+svn export http://dev.wowace.com/wowace/branches/Ace3/WotLK/CallbackHandler-1.0 --force
+svn export http://dev.wowace.com/wowace/trunk/AceGUI-3.0-SharedMediaWidgets --force
+@PAUSE
