5aec417c748a9defb91c5e7632c363427c0a891d
shadowed.wow <shadowed.wow@7083c368-1f31-0410-96f0-4f2793cbc738>
2008-08-30 02:15:55 +0000

    * Initial Import: Bazaar/ * Weird library addon thing that lets you add configuration sharing support to your addons easily. * No GUI yet, but everything should be working internally including progress, the APIs calls and actually sending/receiving data.

---
+++ trunk/Bazaar/libs/update.bat
@@ -0,0 +1,4 @@
+svn export http://dev.wowace.com/wowace/trunk/Ace3/AceSerializer-3.0 --force
+svn export http://dev.wowace.com/wowace/trunk/Ace3/AceComm-3.0 --force
+svn export http://dev.wowace.com/wowace/trunk/Ace3/CallbackHandler-1.0 --force
+@PAUSE
