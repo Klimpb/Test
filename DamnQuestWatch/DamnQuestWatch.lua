49e95a9a7ff73787f5b7b508538dbc69e69bd0dc
shadowed.wow <shadowed.wow@7083c368-1f31-0410-96f0-4f2793cbc738>
2009-04-04 22:23:49 +0000

    * Updated to work with both 3.1 and 3.0

--- DamnQuestWatch/DamnQuestWatch.lua
+++ DamnQuestWatch/DamnQuestWatch.lua
@@ -1,33 +1,42 @@
-local function questWatch_Updated()
-	for i=1, GetNumQuestWatches() do
-		local questID = GetQuestIndexForWatch(i)
-		if( questID ) then
-			local numObjectives = GetNumQuestLeaderBoards(questID)
-		
-			if ( numObjectives > 0 ) then
-				local objsCompleted = 0
-				for j=1, GetNumQuestLeaderBoards(questID) do
-					if( select(3, GetQuestLogLeaderBoard(j, questID)) ) then
-						objsCompleted = objsCompleted + 1
-					end
-				end
-		
-				
-				if( numObjectives == objsCompleted ) then
-					RemoveQuestWatch(questID)
-					QuestWatch_Update()
-					
-					--DEFAULT_CHAT_FRAME:AddMessage("|cFF33FF99QuestWatch|r: " .. GetQuestLogTitle(questID) .. " completed.")
-				end
-			end
-		end
-	end
-end
-
+local IS_31000
 local frame = CreateFrame("Frame")
 frame:RegisterEvent("ADDON_LOADED")
 frame:SetScript("OnEvent", function(self, event, addon)
 	if( addon == "DamnQuestWatch" ) then
-		hooksecurefunc("QuestWatch_Update", questWatch_Updated)
+		self:UnregisterEvent("ADDON_LOADED")
+	
+		local functName
+		if( select(4, GetBuildInfo()) >= 30100 ) then
+			IS_31000 = true
+			functName = "WatchFrame_Update"		
+		else
+			functName = "QuestWatch_Update"	
+		end
+	
+		hooksecurefunc(functName, function()
+			for i=1, GetNumQuestWatches() do
+				local questID = GetQuestIndexForWatch(i)
+				if( questID ) then
+					local numObjectives = GetNumQuestLeaderBoards(questID)
+					if( numObjectives > 0 ) then
+						local completed = 0
+						for j=1, GetNumQuestLeaderBoards(questID) do
+							if( select(3, GetQuestLogLeaderBoard(j, questID)) ) then
+								completed = completed + 1
+							end
+						end
+
+						if( numObjectives == completed ) then
+							RemoveQuestWatch(questID)
+							if( not IS_31000 ) then
+								QuestWatch_Update()
+							end
+							
+							--DEFAULT_CHAT_FRAME:AddMessage("|cFF33FF99QuestWatch|r: " .. GetQuestLogTitle(questID) .. " completed.")
+						end
+					end
+				end
+			end
+		end)
 	end
 end)
